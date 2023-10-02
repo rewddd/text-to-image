cf149fa331de0e2022ea356cfa3f93383d1e6407
Batuhan Taskaya <batuhan@python.org>
2023-10-02 23:26:45 +0300

    feat: bump concurrency to 12

--- text_to_image/model.py
+++ text_to_image/model.py
@@ -180,7 +180,7 @@ def wrap_excs():
     machine_type="GPU",
     keep_alive=4000,
     serve=True,
-    max_concurrency=9,
+    max_concurrency=12,
     _scheduler="distributed",
 )
 def generate_image(input: InputParameters) -> OutputParameters:
